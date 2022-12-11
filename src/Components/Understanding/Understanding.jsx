95b58b6d19db9ee413f3e099c071c587a5a53df5
Pete Hall <pete.mack.hall@gmail.com>
2022-12-11 17:34:56 -0600

    Added next steps

--- src/Components/Understanding/Understanding.jsx
+++ src/Components/Understanding/Understanding.jsx
@@ -8,6 +8,7 @@ export function Understanding() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
+  const feeling = useSelector((state) => state.feeling.value);
   const understanding = useSelector((state) => state.understanding.value);
 
   const [currentUnderstanding, setCurrentUnderstanding] = useState('0');
@@ -18,12 +19,22 @@ export function Understanding() {
     }
   }, [understanding])
 
+  const handleNext = () => {
+    dispatch(updateUnderstanding(Number(currentUnderstanding)));
+    // would send this object to a thunk 
+    // https://redux.js.org/tutorials/essentials/part-5-async-logic
+    console.log({
+      feeling,
+      understanding,
+    })
+  }
+
   return (
     <div className="App">
       <h3>How well are you understanding the content?</h3>
       <button onClick={() => navigate("/feeling")}>BACK</button>
       <input placeholder="Understanding?" type="number" onChange={(e) => setCurrentUnderstanding(e.target.value)} value={currentUnderstanding}/>
-      <button onClick={() => dispatch(updateUnderstanding(Number(currentUnderstanding)))}>NEXT</button>
+      <button onClick={handleNext}>NEXT</button>
     </div>
   );
 }
